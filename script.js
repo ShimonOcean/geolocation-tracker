4e55f2a9079ec2773b86260f22ce5c50588d8d40
ShimonIslam <shimon.ocean@gmail.com>
2021-06-23 21:54:10 -0700

    Add pointers by clicking on map

--- script.js
+++ script.js
@@ -17,9 +17,17 @@ if (navigator.geolocation)
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            L.marker(coords).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+
+            // Leaflet map Object
+            map.on('click', function (mapEvent) {
+                console.log(mapEvent);
+                const { lat, lng } = mapEvent.latlng;
+            
+                L.marker([lat, lng]).addTo(map)
+                .bindPopup('Position')
                 .openPopup();
+            })
+
         }, 
         function() {
             alert('Could not get position');
